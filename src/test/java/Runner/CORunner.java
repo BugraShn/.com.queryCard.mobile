584d228ebc100669dc7499bbd3e5d308f72664c9
onen <onen@gmail.com>
2024-10-15 19:43:49 +0300

    cimen1510

--- src/test/java/Runner/CORunner.java
+++ src/test/java/Runner/CORunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
                 "json:target/cucumber.json",},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@2" ,
+        tags = "@b" ,
         dryRun = false
 
 
