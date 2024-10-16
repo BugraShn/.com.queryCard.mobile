4c29709b8939a00894c0628f664877b1aca3f7d2
Ilgar <ilgar.ganbarov@gmail.com>
2024-10-16 12:58:13 +0200

    changes

--- src/test/java/stepdefinitions/IGStepdefinition.java
+++ src/test/java/stepdefinitions/IGStepdefinition.java
@@ -17,6 +17,7 @@ public class IGStepdefinition extends OptionsMet {
     TouchAction action = new TouchAction<>((PerformsTouchActions) driver);
 
 
+
     @Given("{string} category window and subcategories should be visiblty test")
     public void men_category_window_and_subcategories_should_be_visiblty_test(String menMenu) {
         clickButtonByDescription(menMenu);
@@ -35,7 +36,6 @@ public class IGStepdefinition extends OptionsMet {
     public void the_user_selects_the_product_in_the_men_category_and_goes_down_to_the_text_on_the_page_that_opens(String cardButton) throws InvalidMidiDataException {
         page.MenCardMethod();
 
-
     }
 
     @Given("As a registered user, I want the filtering button to be active and visible")
@@ -47,7 +47,6 @@ public class IGStepdefinition extends OptionsMet {
     @Given("On the My Account page, the user should be able to see the menu titles with his/her information")
     public void on_the_my_account_page_the_user_should_be_able_to_see_the_menu_titles_with_his_her_information() {
         assertTrue(page.myAccountMenu.isDisplayed());
-
     }
 
     @Given("The {string} link should be visible in the profile dashboard sidebar")
